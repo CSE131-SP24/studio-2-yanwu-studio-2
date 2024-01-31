a55ac61ef89453964d62e1c6a8c14f07fede6847
enbunka <enbunka@jana-yans-mbp.dhcp.wustl.edu>
2024-01-31 14:41:25 -0600

    j
    Signed-off-by: enbunka <enbunka@jana-yans-mbp.dhcp.wustl.edu>

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -10,25 +10,22 @@ public class Ruin {
 		double winChance = in.nextDouble();
 		
 		double winlimit = in.nextDouble();
-		
+		for (int totalSimulation = 1, totalSimulation <10, totalSimulation ++) {
 		while (startAmount>0 && winlimit> startAmount) {
-			if (winChance> Math.random()) {
+			if (winChance> Math.random()) 
 				startAmount = startAmount+1;
 			}
 			else {
 				startAmount = startAmount-1;
 			}
 		}
-		if (startAmount >= 100)
-	     {
-	         System.out.println("success");
+		if (startAmount = 0) {
+	         System.out.println("ruin");
 	     }
 	     else 
 	     {
-	         System.out.println("ruin");
+	         System.out.println("success");
 	     }
-	 
-		
 	}
 
 }
